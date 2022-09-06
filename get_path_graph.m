5cc05f61d6f0e2b00c798a151087115b8d4e5c40
Refstop <bhbhchoi@gmail.com>
2022-09-06 15:14:33 +0900

    Add multipath but some issue

--- get_path_graph.m
+++ get_path_graph.m
@@ -1,4 +1,4 @@
-function path_graph = get_path_graph(path_node_idx, i, new_obs, G, goal_pose, culled_k_ls, close_C_ls, r_cost, fig)
+function [path_graph, path_node] = get_path_graph(parent_node, path_node, i, new_obs, G, goal_pose, culled_k_ls, close_C_ls, r_cost, fig)
 if i == culled_k_ls+1
     path_graph = G;
     return;
@@ -27,29 +27,30 @@ if dist < r_cost(i)
     hold on; plot(point_end_avoid(1), point_end_avoid(2),'o','MarkerSize',12);
     
     if new_obs
-        cur_robot_pose = cell2mat(G.Nodes.path(path_node_idx));
+        cur_robot_pose = cell2mat(G.Nodes.path(parent_node));
+        new_obs = false;
     else
         if free_from_obs(culled_k_ls, i, close_C_ls, point_start_avoid, r_cost(i))
             G = addnode(G,1);
-            G.Nodes.path(path_node_idx) = {point_start_avoid};
-            G = addedge(G,path_node_idx-1,path_node_idx);
-            path_node_idx = path_node_idx + 1;
+            G.Nodes.path(path_node) = {point_start_avoid};
+            G = addedge(G,parent_node, path_node);
+            parent_node = path_node;
+            path_node = path_node+1;
             cur_robot_pose = point_start_avoid;
         end
     end
-    
     
 
     % 2. 1번의 시작 지점을 스타트로 inflation radius에 접하는 경로 생성 - 여기서부터 분기점 시작
     sample_num = 100;
     % 현재 로봇의 위치에서 다음 장애물에 대한 접점 2개와 각 접점에 대한 각도를 추출
     [tangential_point, phi] = tangential_lines(close_C_ls(i,:), cur_robot_pose, point_end_avoid, r_cost(i));
-    parent_node_idx = path_node_idx;
+
     branch = [];
     % 두쪽 다에 대해서 검사
     for j = 1:2
         line_sampling = [linspace(cur_robot_pose(1), tangential_point(j,1), sample_num)', linspace(cur_robot_pose(2), tangential_point(j,2), sample_num)'];
-        fprintf("cur: %f, %f\ntan: %f, %f\n\n", cur_robot_pose(1), cur_robot_pose(2), tangential_point(j,1), tangential_point(j,2));
+%         fprintf("cur: %f, %f\ntan: %f, %f\n\n", cur_robot_pose(1), cur_robot_pose(2), tangential_point(j,1), tangential_point(j,2));
         hold on; plot(tangential_point(j,1), tangential_point(j,2), 'o');
         f_check = true;
         for k = 1:sample_num
@@ -61,32 +62,31 @@ if dist < r_cost(i)
         end
         if f_check
             G = addnode(G,1);
-            G.Nodes.path(path_node_idx) = {tangential_point(j,:)};
-            G = addedge(G, parent_node_idx-1, path_node_idx);
-            branch(end+1) = path_node_idx;
-            path_node_idx = path_node_idx+1;
+            G.Nodes.path(path_node) = {tangential_point(j,:)};
+            G = addedge(G, parent_node, path_node);
+            branch(end+1) = path_node;
+            path_node = path_node+1;
         end
     end
 
-    for j = 1:2
-        if isempty(branch)
-            path_graph = G;
-            return;
-        end
+    % 둘 다 갈수 없는 경우 branch가 비어버린다.
+    for j = 1:length(branch)
         cur_robot_pose = cell2mat(G.Nodes.path(branch(j)));
         [tangential_point, phi] = tangential_lines(close_C_ls(i,:), point_end_avoid, cur_robot_pose, r_cost(i));
-        hold on; plot(tangential_point(j,1), tangential_point(j,2), 'o');
 
         % 경로별 매치 필요
+        [argval, argmin] = min([norm(tangential_point(1,:) - cur_robot_pose), norm(tangential_point(2,:) - cur_robot_pose)]);
+        hold on; plot(tangential_point(argmin,1), tangential_point(argmin,2), 'o');
+
         vec1 = cur_robot_pose - close_C_ls(i,:);
-        vec2 = tangential_point(j,:) - close_C_ls(i,:);
+        vec2 = tangential_point(argmin,:) - close_C_ls(i,:);
 
         path_angle = acos(dot(vec1, vec2)/(norm(vec1)*norm(vec2)));
         num = 8; % param
         angle_inc = path_angle/num;
         angle_init = atan2(vec1(2), vec1(1));
 
-        parent_node_idx = branch(j);
+        parent_node = branch(j);
         for l = 1:num
             if j==1 % hard code
                 th_rspl = angle_init - l*angle_inc;
@@ -95,26 +95,30 @@ if dist < r_cost(i)
             end
 
             point_rspl = close_C_ls(i,:) + [r_cost(i)*cos(th_rspl), r_cost(i)*sin(th_rspl)];
-            hold on; plot(point_rspl(1), point_rspl(2), 'o');
             if ~free_from_obs(culled_k_ls, i, close_C_ls, point_rspl, r_avoid)
                 new_obs = true;
+                [G, path_node] = get_path_graph(parent_node, path_node, i+1, new_obs, G, goal_pose, culled_k_ls, close_C_ls, r_cost, fig);
                 break;
             end
+            hold on; plot(point_rspl(1), point_rspl(2), 'o');
             G = addnode(G,1);
-            G.Nodes.path(path_node_idx) = {point_rspl};
-            G = addedge(G, parent_node_idx, path_node_idx);
-            parent_node_idx = path_node_idx;
-            path_node_idx = path_node_idx+1;
+            G.Nodes.path(path_node) = {point_rspl};
+            G = addedge(G, parent_node, path_node);
+            parent_node = path_node;
+            path_node = path_node+1;
         end
-        if ~new_obs
+        fprintf("parent: %d, path: %d\n", parent_node, path_node);
+        if ~new_obs && free_from_obs(culled_k_ls, i, close_C_ls, point_end_avoid, r_avoid)
             G = addnode(G,1);
-            G.Nodes.path(path_node_idx) = {point_end_avoid};
-            G = addedge(G, path_node_idx-1, path_node_idx);
-            path_node_idx = path_node_idx+1;
+            G.Nodes.path(path_node) = {point_end_avoid};
+            G = addedge(G, parent_node, path_node);
+            parent_node = path_node;
+            path_node = path_node+1;
+        else
+            [G, path_node] = get_path_graph(parent_node, path_node, i+1, new_obs, G, goal_pose, culled_k_ls, close_C_ls, r_cost, fig);
         end
+        
     end
-    % 말단 노드에 대해서 graph 생성
-    path_graph = get_path_graph(5, i+1, new_obs, G, goal_pose, culled_k_ls, close_C_ls, r_cost, fig);
-    return;
+    path_graph = G; return;
 end
 end
--- test.m
+++ test.m
@@ -1,7 +1,4 @@
 clc;clear all;close all;
 
-fig1 = figure();
-x=[-5:5];
-y=x;
-plot(x,y);
-figte(fig1);
+[argval, argmin] = min([0.5,1]);
+
